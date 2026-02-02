8653cf8e1994d77211cd9d036da0eed2a092e4e4
rupeshklr1 <rupeshklr1@gmail.com>
2026-02-02 07:39:02 +0530

    ttm

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Check if the environment variable is set to 'true'
-  const isGitHub = process.env.isLiveInGitHUB === 'true';
+  // const isGitHub = process.env.isLiveInGitHUB === 'true';
 
   return {
     plugins: [react()],
     // If live in GitHub, use repo name. If local, use root.
     // REPLACE '/portfolio/' with your actual repo name if different!
-    base: isGitHub ? "/portfolio/" : "/", 
+    // base: isGitHub ? "/portfolio/" : "/", 
   }
 })
